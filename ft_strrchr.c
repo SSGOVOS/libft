96f4197697327829957c209c66c041a98f3a4e09
amoubine <amoubine@c4r6p6.1337.ma>
2023-11-29 01:03:36 +0100

    govos

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: amoubine <amoubine@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 17:40:45 by amoubine          #+#    #+#             */
-/*   Updated: 2023/11/25 02:11:42 by amoubine         ###   ########.fr       */
+/*   Updated: 2023/11/29 00:48:57 by amoubine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,22 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int	len;
+	size_t	len;
 
 	len = ft_strlen(s);
 	while (len > 0)
 	{
-		if (s[len - 1] == (char)c)
-		{
-			return ((char *)(s + len - 1));
-		}
+		if (s[len] == (char)c)
+			return ((char *)(s + len));
 		len--;
 	}
-	len = ft_strlen(s);
-	if (c == 0)
+	if (s[len] == (char)c)
 		return ((char *)(s + len));
 	return (0);
+}
+
+int main()	{
+	char *s = "hello";
+	char *t = ft_strrchr(s, 'o');
+	printf("%s\n", t);
 }
